7f467286cd19346b143de3145f071f4397d76522
Juiceman78 <vincentz78@nycstudents.net>
2025-03-28 10:17:38 -0400

    aaaaaa

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Sample movie data
-movies = [
-    {"id": 1, "title": "Avengers: Endgame", "price": 12},
-    {"id": 2, "title": "Spider-Man: No Way Home", "price": 10},
-    {"id": 3, "title": "Inception", "price": 8}
+things = [
+    {"id": 1, "title": "Volleyball", "description": "I like volleyball"},
+    {"id": 2, "title": "Snowboarding", "description": "I'm tuff at snowboarding"},
+    {"id": 3, "title": "Ashton Hall", "description": "Saratoga Water"}
 ]
 
 # Route for the home page
 @app.route('/')
 def home():
-    return render_template('index.html', movies=movies)
+    return render_template('index.html', things=things) 
 
 
 app.run(debug=True)
